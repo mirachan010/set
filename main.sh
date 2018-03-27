fc8ae914fb8a207b5d520f711248f5942e74952b
mirachan010 <mirachan010@gmail.com>
2018-03-28 08:55:28 +0900

    ちょこっと修正

--- main.sh
+++ main.sh
@@ -1,7 +1,8 @@
 add-apt-repository ppa:jonathonf/vim
 apt upgrade --yes
 apt install vim
+exit
 cd ~/
 git clone http://github.com/mirachan010/.vim.git .vim
-cd ~/.vim/configs/users
-cp .vimrc ~/.vimrc
+cd ~/.vim
+sh ubuntu.sh
