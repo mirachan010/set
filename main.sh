ff79cd9db8fec6506b9af1b1e77ea2bc8ad4c17b
mirachan010 <mirachan010@gmail.com>
2018-04-04 21:41:52 +0900

    gimpインストール

--- main.sh
+++ main.sh
@@ -1,12 +1,24 @@
+#!/bin/sh
+
+#vim8.0用レポジトリ追加
 sudo add-apt-repository ppa:jonathonf/vim
+
+#gimp用レポジトリ追加
+sudo add-apt-repository ppa:otto-kesselgulasch/gimp
+
+#googlechrome用レポジトリ追加
 sudo wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -:
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+
+
+#レポジトリ更新
 sudo apt-get update
 sudo apt update
 sudo apt upgrade --yes
 sudo apt install wine --yes
-sudo apt install google-chrome-stable --yes
+sudo apt install gimp --yes
 sudo apt install vim --yes
+sudo apt install google-chrome-stable --yes
 git clone http://github.com/mirachan010/dotfiles.git dotfiles
 cd ~/dotfiles
 git clone http://github.com/mirachan010/.vim.git .vim
