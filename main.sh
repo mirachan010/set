f8849f35dc36ede723a365ce49f6f11d85042059
mirachan010 <mirachan010@gmail.com>
2018-04-04 17:27:49 +0900

    起動シェル名を変更

--- main.sh
+++ main.sh
@@ -6,5 +6,5 @@ sudo apt install vim --yes
 git clone http://github.com/mirachan010/dotfiles.git dotfiles
 cd ~/dotfiles
 git clone http://github.com/mirachan010/.vim.git .vim
-sh ubuntu.sh
+sh dots.sh
 vim
