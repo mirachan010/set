f79cd35586d617fdbf302bddd95b87f7b6e759f7
mirachan010 <mirachan010@gmail.com>
2018-04-04 17:46:45 +0900

    2 : ; fadf dasf xsdfsefewafaaedgadfwg

--- main.bat
+++ main.bat
@@ -1,8 +1,10 @@
+@powershell -NoProfile -ExecutionPolicy unrestricted -Command "Start-Process powershell.exe -Verb runas"
 Set-ExecutionPolicy RemoteSigned
  iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco upgrade chocolatey --yes
 choco install package.config --yes
 cd ~/
+git clone https://github.com/mirachan010/dotfiles.git
+cd dotfiles
 git clone https://github.com/mirachan010/.vim.git .vim
-cd .vim/configs/users
-cp .vimrc ~/.vimrc
+sh dots.sh
