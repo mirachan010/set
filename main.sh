c93c7a08168898133d522f2654204ea37566acfb
mirachan010 <mirachan010@gmail.com>
2018-04-04 16:53:08 +0900

    dotfiles

--- main.sh
+++ main.sh
@@ -3,7 +3,7 @@ sudo apt update
 sudo apt upgrade --yes
 sudo apt install wine --yes
 sudo apt install vim --yes
-cd ~/
+git clone http://github.com/mirachan010/dotfiles.git dotfiles
+cd ~/dotfiles
 git clone http://github.com/mirachan010/.vim.git .vim
-cd ~/.vim
 sh ubuntu.sh
