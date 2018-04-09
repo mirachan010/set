059312a4bf97fc2bd1aa661179df4f972ea8df31
mira <37107329+mirachan010@users.noreply.github.com>
2018-04-09 15:40:13 +0900

    googlechromeインストールされないように
    なんか入らなかったみたいだからじぶんでいれる！！

--- main.sh
+++ main.sh
@@ -9,10 +9,6 @@ sudo add-apt-repository ppa:jonathonf/vim
 #gimp用レポジトリ追加
 sudo add-apt-repository ppa:otto-kesselgulasch/gimp
 
-#googlechrome用レポジトリ追加
-sudo wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -:
-sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-
 
 #レポジトリ更新
 sudo apt-get update
@@ -22,7 +18,6 @@ sudo apt install easystroke --yes
 sudo apt install wine --yes
 sudo apt install gimp --yes
 sudo apt install vim --yes
-sudo apt install google-chrome-stable --yes
 
 #dotfilesの設定とか
 git clone http://github.com/mirachan010/dotfiles.git dotfiles
