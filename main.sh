38ac1bd586e64bbf5d904f658fbdd45e884cdc89
mirachan010 <mirachan010@gmail.com>
2018-11-23 15:34:19 +0900

    とりあえずさん種類のファイルに分けてみたzshの再起動を通すからこうしたけどもっとかんたんにまとめれたりする？

--- main.sh
+++ main.sh
@@ -4,21 +4,26 @@
 cd ~/
 
 #gimp用レポジトリ追加
+sudo apt-get install software-properties-common
 sudo add-apt-repository ppa:otto-kesselgulasch/gimp
-
+sudo apt-add-repository ppa:neovim-ppa/stable  
 
 #レポジトリ更新
-sudo apt-get update
 sudo apt update
 sudo apt upgrade --yes
-sudo apt install default-jdk --yes
-sudo apt install easystroke --yes
-sudo apt install wine --yes
-sudo apt install gimp --yes
-sudo apt install chrome-gnome-shell
-sudo apt install gnome-tweak-tool
+sudo apt install -y default-jdk terminater git zsh curl wine gimp chrome-gnome-shell gnome-tweak-tool make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev neovim
+
+#Qiitaに上げていた分
+chsh -s /usr/bin/zsh
+curl -sL --protoredir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
+git clone git://github.com/yyuu/pyenv.git ~/.pyenv
+
+cd /usr/share/fonts/truetype
+fc-cache -vf
+sudo git clone https://github.com/mzyy94/RictyDiminished-for-Powerline.git powerline
+
+
 #dotfilesの設定とか
 git clone http://github.com/mirachan010/dotfiles.git dotfiles
 cd ~/dotfiles
 sh dots.sh
-chmod +x ~/dotfiles/main.sh
---
+++ main2.sh
@@ -0,0 +1,8 @@
+pyenv install 3.7.1
+pyenv insall 2.7.15
+pyenv global 3.7.1 2.7.15
+pip2 install --upgrade pip
+pip install --upgrade pip
+pip install neovim
+pip install --user powerline-shell
+pip install --user powerline-status
