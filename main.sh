f2611559cdf78018d4fab7ecc016508765b89442
mirachan010 <mirachan010@gmail.com>
2018-12-22 11:43:08 +0900

    違和感のあったとこの修復
    wineなくてもいいしょ

--- main.sh
+++ main.sh
@@ -11,12 +11,12 @@ sudo apt-add-repository ppa:neovim-ppa/stable
 #レポジトリ更新
 sudo apt update
 sudo apt upgrade --yes
-sudo apt install -y default-jdk terminater git zsh curl wine gimp chrome-gnome-shell gnome-tweak-tool make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev neovim
+sudo apt install -y default-jdk terminator tmux git zsh curl gimp chrome-gnome-shell gnome-tweak-tool make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev neovim
 
 #Qiitaに上げていた分
 chsh -s /usr/bin/zsh
-curl -sL --protoredir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 git clone git://github.com/yyuu/pyenv.git ~/.pyenv
+curl -sL --protoredir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 
 cd /usr/share/fonts/truetype
 sudo git clone https://github.com/mzyy94/RictyDiminished-for-Powerline.git powerline
--- main2.sh
+++ main2.sh
@@ -1,5 +1,5 @@
 pyenv install 3.7.1
-pyenv insall 2.7.15
+pyenv install 2.7.15
 pyenv global 3.7.1 2.7.15
 pip2 install --upgrade pip
 pip install --upgrade pip
