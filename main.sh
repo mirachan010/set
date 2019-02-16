07ffeee4673af06aced23ee66f7483f81ba1f719
mirachan010 <mirachan010@gmail.com>
2019-02-16 14:31:38 +0900

    だいぶ修正

--- main.sh
+++ main.sh
@@ -11,7 +11,7 @@ sudo apt-add-repository ppa:neovim-ppa/stable
 #レポジトリ更新
 sudo apt update
 sudo apt upgrade --yes
-sudo apt install -y default-jdk terminator tmux git zsh curl gimp chrome-gnome-shell gnome-tweak-tool make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev neovim
+sudo apt install -y default-jdk terminator tmux git zsh curl gimp chrome-gnome-shell gnome-tweak-tool make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev neovim silversearcher-ag
 
 #Qiitaに上げていた分
 chsh -s /usr/bin/zsh
@@ -25,6 +25,7 @@ fc-cache -vf
 
 
 #dotfilesの設定とか
-git clone http://github.com/mirachan010/dotfiles.git dotfiles
+cd ~/
+git clone git@github.com:mirachan010/dotfiles
 cd ~/dotfiles
 sh dots.sh
--- main2.sh
+++ main2.sh
@@ -4,6 +4,7 @@ pyenv global 3.7.1 2.7.15
 pip2 install --upgrade pip
 pip install --upgrade pip
 pip install neovim
+pip2 insatll neovim
 pip install --user powerline-shell
 pip install --user powerline-status
 nvim
